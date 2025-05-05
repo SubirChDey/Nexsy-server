b26946a9e3125fc0135df6491fe37b9f57715186
Subir Chandra Dey <subirdey93@gmail.com>
2025-05-05 14:37:20 +0600

    users data saved by googlelogin

--- index.js
+++ index.js
@@ -88,6 +88,11 @@ async function run() {
     // save users data
     app.post('/users', async(req, res) => {
       const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser) {
+        return res.send({message: 'user already exists', insertedId: null})
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
